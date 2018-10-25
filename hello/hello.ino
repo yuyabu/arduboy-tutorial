d6a88fa48c0a4a618e84fbebf40fbcde03af2d99
yuyabu <yuyabushita@gmail.com>
2018-10-26 01:00:39 +0900

    add hello world sample

---
+++ hello/hello.ino
@@ -0,0 +1,15 @@
+#include <Arduboy.h>
+Arduboy arduboy;
+
+void setup() {
+  // put your setup code here, to run once:
+  arduboy.begin();
+  arduboy.clear();
+  arduboy.print("Hello yuyabu World");
+  arduboy.display();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
